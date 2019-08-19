2950ad62494c66e74c6d317aca85e9c5d00a527b
#Momo <40578274+MomoDeve@users.noreply.github.com>
2019-08-20 00:00:44 +0300

    Add files via upload

---
+++ src/CircuitCalculator.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace CircuitCalculator
+{
+    partial class CircuitCalculator
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(CircuitCalculator));
+            this.ComponentsList = new System.Windows.Forms.RichTextBox();
+            this.CraftRecursion = new System.Windows.Forms.CheckBox();
+            this.MaxTierCircuits = new System.Windows.Forms.CheckBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.ResetButton = new System.Windows.Forms.Button();
+            this.StackButton = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // ComponentsList
+            // 
+            this.ComponentsList.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.ComponentsList.Location = new System.Drawing.Point(723, 11);
+            this.ComponentsList.Name = "ComponentsList";
+            this.ComponentsList.ReadOnly = true;
+            this.ComponentsList.Size = new System.Drawing.Size(529, 658);
+            this.ComponentsList.TabIndex = 12;
+            this.ComponentsList.Text = "";
+            // 
+            // CraftRecursion
+            // 
+            this.CraftRecursion.AutoSize = true;
+            this.CraftRecursion.Checked = true;
+            this.CraftRecursion.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.CraftRecursion.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.CraftRecursion.Location = new System.Drawing.Point(542, 13);
+            this.CraftRecursion.Name = "CraftRecursion";
+            this.CraftRecursion.Size = new System.Drawing.Size(164, 28);
+            this.CraftRecursion.TabIndex = 13;
+            this.CraftRecursion.Text = "Recursive Crafts";
+            this.CraftRecursion.UseVisualStyleBackColor = true;
+            this.CraftRecursion.CheckedChanged += new System.EventHandler(this.CraftRecursion_CheckedChanged);
+            // 
+            // MaxTierCircuits
+            // 
+            this.MaxTierCircuits.AutoSize = true;
+            this.MaxTierCircuits.Checked = true;
+            this.MaxTierCircuits.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.MaxTierCircuits.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.MaxTierCircuits.Location = new System.Drawing.Point(542, 42);
+            this.MaxTierCircuits.Name = "MaxTierCircuits";
+            this.MaxTierCircuits.Size = new System.Drawing.Size(170, 28);
+            this.MaxTierCircuits.TabIndex = 14;
+            this.MaxTierCircuits.Text = "Max-Tier Circuits";
+            this.MaxTierCircuits.UseVisualStyleBackColor = true;
+            this.MaxTierCircuits.CheckedChanged += new System.EventHandler(this.HighTierCircuits_CheckedChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(12, 645);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(293, 24);
+            this.label1.TabIndex = 15;
+            this.label1.Text = "GTNH Circuit Calculator by Momo";
+            // 
+            // ResetButton
+            // 
+            this.ResetButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.ResetButton.Location = new System.Drawing.Point(542, 110);
+            this.ResetButton.Name = "ResetButton";
+            this.ResetButton.Size = new System.Drawing.Size(164, 31);
+            this.ResetButton.TabIndex = 16;
+            this.ResetButton.Text = "Reset";
+            this.ResetButton.UseVisualStyleBackColor = true;
+            this.ResetButton.Click += new System.EventHandler(this.ResetButton_Click);
+            // 
+            // StackButton
+            // 
+            this.StackButton.AutoSize = true;
+            this.StackButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.StackButton.Location = new System.Drawing.Point(542, 76);
+            this.StackButton.Name = "StackButton";
+            this.StackButton.Size = new System.Drawing.Size(178, 28);
+            this.StackButton.TabIndex = 17;
+            this.StackButton.Text = "Count Item Stacks";
+            this.StackButton.UseVisualStyleBackColor = true;
+            this.StackButton.CheckedChanged += new System.EventHandler(this.StackButton_CheckedChanged);
+            // 
+            // CircuitCalculator
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1264, 681);
+            this.Controls.Add(this.StackButton);
+            this.Controls.Add(this.ResetButton);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.MaxTierCircuits);
+            this.Controls.Add(this.CraftRecursion);
+            this.Controls.Add(this.ComponentsList);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "CircuitCalculator";
+            this.Text = "Circuit Calculator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.RichTextBox ComponentsList;
+        private System.Windows.Forms.CheckBox CraftRecursion;
+        private System.Windows.Forms.CheckBox MaxTierCircuits;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button ResetButton;
+        private System.Windows.Forms.CheckBox StackButton;
+    }
+}
+
---
+++ src/CircuitCalculator.cs
@@ -0,0 +1,414 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace CircuitCalculator
+{
+    public partial class CircuitCalculator : Form
+    {
+        readonly string electronicCircuit = "Electronic Circuit";
+        readonly string integratedCircuit = "Integrated Logic Circuit";
+        readonly string goodElectronicCircuit = "Good Electronic Circuit";
+        readonly string goodIntegratedCircuit = "Good Integrated Circuit";
+        readonly string advancedCircuit = "Advanced Circuit";
+        readonly string integratedProcessor = "Integrated Processor";
+        readonly string processorAssembly = "Processor Assembly";
+        readonly string nanoprocessor = "Nanoprocessor";
+        readonly string workstation = "Workstation";
+        readonly string nanoprocessorAssembly = "Nanoprocessor Assembly";
+        readonly string quantumprocessor = "Quantumprocessor";
+        readonly string quantumprocessorAssembly = "Quantumprocessor Assembly";
+        readonly string eliteNanocomputer = "Elite Nanocomputer";
+        readonly string mainframe = "Mainframe";
+        readonly string crystalprocessor = "Crystalprocessor";
+
+        List<NumericUpDown> Numerics = new List<NumericUpDown>();
+
+        public CircuitCalculator()
+        {
+            InitializeComponent();
+
+            #region CIRCUITS
+            Circuits.Add(electronicCircuit, new Circuit()
+            {
+                Components = new Dictionary<string, int>()
+                {
+                    { "Circuit Board", 1 },
+                    { "SMD Resistor", 2 },
+                    { "1x Red Alloy Wire", 2 },
+                    { "Vaccum Tube", 2 }
+                }
+            });
+
+            Circuits.Add(integratedCircuit, new Circuit()
+            {
+                Components = new Dictionary<string, int>()
+                {
+                    { "Circuit Board", 1 },
+                    { "SMD Resistor", 2 },
+                    { "SMD Diode", 2 },
+                    { "Fine Copper Wire", 2 },
+                    { "Tin Bolt", 2 }
+                }
+            });
+
+            Circuits.Add(goodElectronicCircuit, new Circuit()
+            {
+                Components = new Dictionary<string, int>()
+                {
+                    { integratedCircuit, 2 },
+                    { "Good Circuit Board", 1 },
+                    { "SMD Diode", 2 },
+                    { "1x Copper Wire", 2 }
+                }
+             });
+
+            Circuits.Add(goodIntegratedCircuit, new Circuit()
+            {
+                Components = new Dictionary<string, int>()
+                {
+                    { integratedCircuit, 2 },
+                    { "Good Circuit Board", 1 },
+                    { "SMD Resistor", 4 },
+                    { "SMD Diode", 4 },
+                    { "Fine Gold Wire", 4 },
+                    { "Silver Bolt", 4 }
+                }
+            });
+
+            Circuits.Add(advancedCircuit, new Circuit()
+            {
+                Components = new Dictionary<string, int>()
+                {
+                    { goodIntegratedCircuit, 1 },
+                    { "Integrated Logic Circuit (Wafer-made)", 2 },
+                    { "Random Access Memory Chip", 2 },
+                    { "SMD Transistor", 4 },
+                    { "Fine Electrum Wire", 8 },
+                    { "Annealed Copper Bolt", 8 }
+                }
+            });
+
+            Circuits.Add(integratedProcessor, new Circuit()
+            {
+                Components = new Dictionary<string, int>()
+                {
+                    { "Plastic Circuit Board", 1 },
+                    { "Central Processing Unit", 1},
+                    { "SMD Resistor", 4 },
+                    { "SMD Capacitor", 4 },
+                    { "SMD Transistor", 4 },
+                    { "Fine Red Alloy Wire", 4 }
+                }
+            });
+
+            Circuits.Add(processorAssembly, new Circuit()
+            {
+                Components = new Dictionary<string, int>()
+                {
+                    { integratedProcessor, 2 },
+                    { "Plastic Circuit Board", 1 },
+                    { "Small Coil", 4 },
+                    { "SMD Capacitor", 8 },
+                    { "Random Access Memory Chip", 4 },
+                    { "Fine Red Alloy Wire", 4 }
+                }
+            });
+
+            Circuits.Add(workstation, new Circuit()
+            {
+                Components = new Dictionary<string, int>()
+                {
+                    { processorAssembly, 2 },
+                    { "Plastic Circuit Board", 1 },
+                    { "SMD Diode", 4 },
+                    { "Random Access Memory Chip", 4 },
+                    { "Fine Electrum Wire", 16 },
+                    { "Platinum Bolt", 16 }
+                }
+            });
+
+            Circuits.Add(mainframe, new Circuit()
+            {
+                Components = new Dictionary<string, int>()
+                {
+                    { workstation, 2 },
+                    { "Aluminium Frame Box", 2 },
+                    { "Small Coil", 12 },
+                    { "SMD Capacitor", 16 },
+                    { "Random Access Memory Chip", 16 },
+                    { "1x Annealed Copper Wire", 16 },
+                }
+            });
+
+            Circuits.Add(nanoprocessor, new Circuit()
+            {
+                Components = new Dictionary<string, int>()
+                {
+                    { "Advanced Circuit Board", 1 },
+                    { "ASoC", 1 },
+                    { "Fine Electrum Wire", 8 },
+                    { "Platinum Bolt", 8 }
+                }
+            });
+
+            Circuits.Add(nanoprocessorAssembly, new Circuit()
+            {
+                Components = new Dictionary<string, int>()
+                {
+                    {nanoprocessor, 2 },
+                    { "Advanced Circuit Board", 1 },
+                    { "Small Coil", 8 },
+                    { "SMD Capacitor", 8 },
+                    { "Random Access Memory Chip", 8 },
+                    { "Fine Electrum Wire", 16 },
+                }
+            });
+
+            Circuits.Add(eliteNanocomputer, new Circuit()
+            {
+                Components = new Dictionary<string, int>()
+                {
+                    { nanoprocessorAssembly, 2 },
+                    { "Advanced Circuit Board", 1 },
+                    { "SMD Diode", 8 },
+                    { "NOR Memory Chip", 4 },
+                    { "Random Access Memory Chip", 16 },
+                    { "Fine Electrum Wire", 16 },
+                }
+            });
+
+            Circuits.Add(quantumprocessor, new Circuit()
+            {
+                Components = new Dictionary<string, int>()
+                {
+                    { "More Advanced Circuit Board", 1 },
+                    { "ASoC", 1 },
+                    { "Fine Electrum Wire", 16 },
+                    { "Niobium-Titanum Bolt", 8 }
+                }
+            });
+
+            Circuits.Add(quantumprocessorAssembly, new Circuit()
+            {
+                Components = new Dictionary<string, int>()
+                {
+                    { quantumprocessor, 2 },
+                    { "Advanced Circuit Board", 1 },
+                    { "Small Coil", 12 },
+                    { "SMD Capacitor", 16 },
+                    { "Random Access Memory Chip", 4 },
+                    { "Fine Platinum Wire", 24 },
+                }
+            });
+
+            Circuits.Add(crystalprocessor, new Circuit()
+            {
+                Components = new Dictionary<string, int>()
+                {
+                    { "Elite Circuit Board", 1 },
+                    { "Crystal SoC", 1 },
+                    { "Fine Niobium-Titanum Wire", 8 },
+                    { "Yttrium Barium Cuprate Bolt", 8 },
+                }
+            });
+            #endregion
+
+            int tabIndex = 0;
+            int yOffset = 0;
+            int xOffset = 10;
+            int offsetDelta = 25;
+            int elementWidth = 250;
+            foreach(var pair in Circuits)
+            {
+                if(yOffset > 350)
+                {
+                    yOffset = 0;
+                    xOffset += elementWidth + 25;
+                }
+                var label = new Label()
+                {
+                    Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204))),
+                    Location = new System.Drawing.Point(xOffset, yOffset),
+                    Size = new System.Drawing.Size(elementWidth, offsetDelta),
+                    Text = pair.Key,
+                    TabIndex = tabIndex++,
+                };
+                Controls.Add(label);
+                yOffset += offsetDelta;
+                var numeric = new NumericUpDown()
+                {
+                    Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204))),
+                    Location = new System.Drawing.Point(xOffset, yOffset),
+                    Size = new System.Drawing.Size(elementWidth, offsetDelta),
+                    Name = pair.Key,
+                    TabIndex = tabIndex++,
+                    Maximum = 65536
+                };
+                Numerics.Add(numeric);
+                numeric.ValueChanged += new System.EventHandler(CalculateAllCircuits);
+                Controls.Add(numeric);
+                yOffset += offsetDelta;
+            }
+        }
+
+        Dictionary<string, int> Components = new Dictionary<string, int>();
+        Dictionary<string, Circuit> Circuits = new Dictionary<string, Circuit>();
+
+        private void AddToList(Circuit circuit)
+        {
+            if (circuit.Amount == 0) return;
+
+            foreach (var pair in circuit.Components)
+            {
+                if(CraftRecursion.Checked && Circuits.ContainsKey(pair.Key))
+                {
+                    Circuits[pair.Key].Amount += pair.Value * circuit.Amount;
+                    AddToList(Circuits[pair.Key]);
+                }
+                else
+                {
+                    if (!Components.ContainsKey(pair.Key)) Components.Add(pair.Key, 0);
+                    Components[pair.Key] += pair.Value * circuit.Amount;
+                }
+            }
+        }
+
+        private void UpdateComponentsList()
+        {
+            ComponentsList.Clear();
+            foreach (var pair in Components)
+            {
+                if (pair.Value > 0)
+                {
+                    if (StackButton.Checked)
+                    {
+                        int stacks = pair.Value / 64;
+                        int mod = pair.Value % 64;
+                        if(stacks > 0 && mod > 0)
+                        {
+                            ComponentsList.Text +=stacks.ToString() + " * 64 + " + mod.ToString() + " " + pair.Key + '\n';
+                        }
+                        else if (stacks == 0)
+                        {
+                            ComponentsList.Text += mod.ToString() + " " + pair.Key + '\n';
+                        }
+                        else
+                        {
+                            ComponentsList.Text += stacks.ToString() + " * 64 " + pair.Key + '\n';
+                        }
+                    }
+                    else
+                    {
+                        ComponentsList.Text += pair.Value.ToString() + "x " + pair.Key + '\n';
+                    }
+                }
+            }
+        }
+
+        private void CalculateAllCircuits(object sender, EventArgs e)
+        {
+            Components.Clear();
+
+            foreach(var numeric in Numerics)
+            {
+                Circuits[numeric.Name].Amount = (int)numeric.Value;
+            }
+
+            foreach(var pair in Circuits)
+            {
+                AddToList(pair.Value);
+            }
+
+            UpdateComponentsList();
+        }
+
+        private void HighTierCircuits_CheckedChanged(object sender, EventArgs e)
+        {
+            if(MaxTierCircuits.Checked)
+            {
+                Circuits[goodElectronicCircuit].Components.Remove(electronicCircuit);
+                Circuits[goodElectronicCircuit].Components.Add(integratedCircuit, 2);
+
+                Circuits[nanoprocessor].Components = new Dictionary<string, int>()
+                {
+                    { "Advanced Circuit Board", 1 },
+                    { "ASoC", 1 },
+                    { "Fine Electrum Wire", 8 },
+                    { "Platinum Bolt", 8 }
+                };
+
+                Circuits[quantumprocessor].Components = new Dictionary<string, int>()
+                {
+                    { "More Advanced Circuit Board", 1 },
+                    { "ASoC", 1 },
+                    { "Fine Electrum Wire", 16 },
+                    { "Niobium-Titanum Bolt", 8 }
+                };
+
+                Circuits[crystalprocessor].Components = new Dictionary<string, int>()
+                {
+                    { "Elite Circuit Board", 1 },
+                    { "Crystal SoC", 1 },
+                    { "Fine Niobium-Titanum Wire", 8 },
+                    { "Yttrium Barium Cuprate Bolt", 8 },
+                };
+            }
+            else
+            {
+                Circuits[goodElectronicCircuit].Components.Remove(integratedCircuit);
+                Circuits[goodElectronicCircuit].Components.Add(electronicCircuit, 2);
+
+                Circuits[nanoprocessor].Components = new Dictionary<string, int>()
+                {
+                    { "Advanced Circuit Board", 1 },
+                    { "Nanocomponent Central Processing Unit", 1 },
+                    { "SMD Resistor", 8 },
+                    { "SMD Capacitor", 8 },
+                    { "SMD Transistor", 8 },
+                    { "Fine Electrum Wire", 8 }
+                };
+
+                Circuits[quantumprocessor].Components = new Dictionary<string, int>()
+                {
+                    { "More Advanced Circuit Board", 1 },
+                    { "QBit Processing Unit", 1 },
+                    { "Nanocomponent Central Processing Unit", 1 },
+                    { "SMD Capacitor", 12 },
+                    { "SMD Transistor", 12 },
+                    { "Fine Platinum Wire", 16 }
+                };
+
+                Circuits[crystalprocessor].Components = new Dictionary<string, int>()
+                {
+                    { "Elite Circuit Board", 1 },
+                    { "Crystal Processing Unit", 1 },
+                    { "Nanocomponent Central Processing Unit", 2 },
+                    { "SMD Capacitor", 24 },
+                    { "SMD Transistor", 24 },
+                    { "Fine Niobium-Titanum Wire", 8 },
+                };
+            }
+            CalculateAllCircuits(sender, e);
+        }
+
+        private void CraftRecursion_CheckedChanged(object sender, EventArgs e)
+        {
+            CalculateAllCircuits(sender, e);
+        }
+
+        private void ResetButton_Click(object sender, EventArgs e)
+        {
+            foreach(var numeric in Numerics)
+            {
+                numeric.Value = 0;
+            }
+            CalculateAllCircuits(sender, e);
+        }
+
+        private void StackButton_CheckedChanged(object sender, EventArgs e)
+        {
+            CalculateAllCircuits(sender, e);
+        }
+    }
+}
