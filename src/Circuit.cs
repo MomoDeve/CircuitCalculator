13e75c346df51ad071e57d9c098cacb7b8747bd1
#Momo <40578274+MomoDeve@users.noreply.github.com>
2019-08-19 23:58:35 +0300

    Create Circuit.cs

---
+++ src/Circuit.cs
@@ -0,0 +1,11 @@
+using System.Collections.Generic;
+
+namespace CircuitCalculator
+{
+    class Circuit
+    {
+        public Dictionary<string, int> Components { get; set; }
+
+        public int Amount { get; set; }
+    }
+}
